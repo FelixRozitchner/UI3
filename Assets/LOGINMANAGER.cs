b182a20ef3ec22a5770c9487fda0611cdb73e094
FelixRozitchner <frozitchner@gmail.com>
2024-05-27 15:49:36 -0300

    eac

--- Assets/LOGINMANAGER.cs
+++ Assets/LOGINMANAGER.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+//Cuando el usuario presione el BtnAceptar debe escribirse un mensaje en la consola, si el texto del inputPassword
+//coincide con una contraseña predefinida por ustedes escribir "Access Granted" sino escribir "Access Denied"
 public class LOGINMANAGER : MonoBehaviour
 {
+    string contraseña = "hola123";
+    public InputField inputPassword;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,17 @@ public class LOGINMANAGER : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void chequear()
+    {
+        if(inputPassword.text == contraseña)
+        {
+            Debug.Log("ACCESS GRANTED");
+        }
+        else
+        {
+            Debug.Log("ACCESS DENIED");
+        }
     }
 }
